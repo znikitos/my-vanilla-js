1a15593eab8c1a3d9cb3518e3db34b581e1ec1b0
Nikita <shimin.nikita@gmail.com>
2020-08-01 09:06:31 +0300

    init project

--- src/test.js
+++
@@ -1,6 +0,0 @@
-const k = "sadsad";
-console.log(k);
-
-for (let i = 0; i < 12; i += 1) {
-  console.log(i);
-}
---
+++ test.js
@@ -0,0 +1,27 @@
+// import fetch from "node-fetch";
+
+const k = "sadsad";
+console.log(k);
+
+// let res = fetch('https://api.github.com/users/nikitos/repos');
+// res.then((re) => {
+//   console.log(re);
+// }).then((data)=>{
+//   console.log(data);
+
+// });
+// const test = async () => {
+//   const res = await fetch("https://api.github.com/users/nikitos/repos");
+//   const data = await res.json();
+//   console.log(data);
+// };
+
+console.log("ssss");
+const res = fetch("https://api.github.com/users/nidfsdfsfsfsd");
+res.then((resNext) => resNext.json()).then((data) => console.log(data));
+
+alert("gere");
+
+// for (let i = 0; i < 12; i += 1) {
+//   console.log(i);
+// }
